ebf7cb7994c42f4d1483553c5d891f0a87b6d48e
Thierry <thierry.ndayegamiye@gmail.com>
2020-05-12 12:09:06 +0200

    rollback + asset

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,20 +12,17 @@
 <!-- jQuery -->
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<<<<<<< HEAD:resources/views/partials/footer.blade.php
-<script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables -->
-<script src="/adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<!-- AdminLTE App -->
-<script src="/adminlte/js/adminlte.min.js"></script>
 
-=======
+<script src="{{ asset('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- DataTables -->
+<script src="{{ asset('/adminlte/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{ asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{ asset('/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('/adminlte/js/adminlte.min.js')}}"></script>
 <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('adminlte/js/adminlte.min.js') }}"></script>
->>>>>>> master:resources/views/dashboard/partials/footer.blade.php
 </body>
 </html>
