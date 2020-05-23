a7df2dd0b5bd35066157bdfd58b9f9fa4ed217d2
Thierry <thierry.ndayegamiye@gmail.com>
2020-05-23 14:57:47 +0200

    affichage hti

--- resources/views/interaction/index_mecanisme.blade.php
+++ resources/views/interaction/index_mecanisme.blade.php
@@ -4,10 +4,8 @@
 
 @section('content_dashboard')
 	<div class="row">
-		<div class="col-md-1">
-
-		</div>
-       <div class="col-md-6 .offset-md-1 ">
+		<div class="col-md-1"></div>
+       <div class="col-md-5 .offset-md-1 ">
            <form action="">
 				<fieldset class="form-group">
 					<legend style="color: #e32; font-size: 160%; font-weight: bold" >Interactions...</legend>
@@ -54,10 +52,77 @@
 							</div>
 						</div>
 					</div>
-					<button type="submit" class="btn btn-primary" style="background-color: #62af56; border: 1px solid #62af56">chercher...</button>
+					<button type="submit" class="btn btn-primary" style="background-color: #62af56; border: 1px solid #62af56">
+						Chercher &nbsp;&nbsp;<span><i class="fas fa-chevron-right "></i></span>
+					</button>
 				</fieldset>
 		   </form>
 	   </div>
+	   <div class="col-md-3"></div>
+	   <div class="col-md-2 text-center">
+		   <h3 style="color: #777;font-family: 'Gill Sans','lucida grande', helvetica, arial, sans-serif;font-size: 160%;font-weight: bold">Intensité d'interaction</h3>
+		   <div class="table-responsive-sm">
+			<table class="table table-bordered table-hover table-sm">
+			   <tbody>
+				   <tr style="background-color: #FF0000">
+					   <th>Forte</th>
+				   </tr>
+				   <tr style="background-color: #FFCC33">
+					   <th>Moyenne</th>
+				   </tr>
+				   <tr  style="background-color: #EEE8AA">
+					   <th>Faible</th>
+				   </tr>
+				   <tr  style="background-color: #CCFF99">
+					   <th>Aucune</th>
+				   </tr>
+				   <tr  style="background-color: #FFCCFF">
+					   <th>Inconnue</th>
+				   </tr>
+				   <tr  style="background-color: #FF0000">
+					   <th>G4</th>
+				   </tr>
+				   <tr  style="background-color: #F46207">
+					   <th>G3</th>
+				   </tr>
+				   <tr  style="background-color: #FFCC33">
+					   <th>G2</th>
+				   </tr>
+				   <tr  style="background-color: #EEE8AA"> 
+					   <th>G1</th>
+				   </tr>
+				   <tr  style="background-color: #CCFF99">
+					<th>G0</th>
+
+				   </tr>
+
+			   </tbody>
+			</table>
+		  </div>
+	   </div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-1"></div>
+      <div class="col-md-8">
+		<hr>
+			<div class="row">
+               <div class="col-md-6">
+				   <h1 style="color: red"> Cas Cliniques</h1>
+				   <p>
+					   Aucune étude ni cas référencé
+				   </p>
+			   </div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h1 style="color: red"> Interactions Potentielles</h1>
+				   <p>
+					   Aucune interaction référencée
+				   </p>
+				</div>
+			 </div>
+	  </div>
 	</div>
 
 @endsection
