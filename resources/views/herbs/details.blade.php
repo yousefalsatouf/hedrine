7175a6c8e6b7b5f2e2ccae94ea097de095ed8b51
jpmpindu <jpmpindu@gmail.com>
2020-05-26 11:00:05 +0200

    Verification des modifs

--- resources/views/herbs/details.blade.php
+++ resources/views/herbs/details.blade.php
@@ -43,28 +43,7 @@
 			</div>
 		</div>
 <div class="col-12">
-	<div class="card-body " style="background-color: #fff">
-		
-		<table id="example1" class="table table-bordered table-striped">
-			<thead>
-				<tr>
-					<th>  Nom </th>
-					<th>  Nom scientifique</th>
-				</tr>
-			</thead>
-			<tbody>
-				
-				<tr>
-					<td>
-						{{$informations_plante[0]->hname}}
-					</td>
-					<td>
-						{{$informations_plante[0]->sciname}}
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+	
    <br>
 	<section>
 		<div class="container-fluid">
@@ -144,47 +123,7 @@
 @section('dashboard-js')
 <script>
 	$(function () {
-<div class="col-12">
-	<div class="card-body " style="background-color: #fff">
-		<table id="example1" class="table table-bordered table-striped">
-			<thead>
-				<tr>
-					<th> </th>
-					<th> Effets</th>
-					<th> Intensité</th>
-					<th> Note</th>
-					<th> Références</th>
-				</tr>
-			</thead>
-			<tbody>
-				
-				@foreach($informations_plante as $information_plante)
-				<tr>
-					<td>
-						{{$information_plante->targetname}}
-					</td>
-					<td>
-						@foreach($hinteractions_has_effects as $hinteraction_has_effects)
-							{{-- {{ $hinteraction_has_effects->effect_id }}	 --}}
-						@endforeach
-					</td>
-					<td>
-						
-							{{$information_plante->force_name}}
-						
-					</td>
-					<td>
-						{{$information_plante->hinteractionnote}}
-					</td>
-					<td>
-						
-					</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
- </div>
+
 
 	
 	  $('#example1').DataTable({
