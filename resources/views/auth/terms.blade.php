3fd34465aff750b080d4b2af7d1a592f1ba5a2e4
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-06-13 00:14:53 +0200

    login and register  styles are changed, the disable button is set until checkout and accept conditions

--- resources/views/auth/terms.blade.php
+++ resources/views/auth/terms.blade.php
@@ -1,29 +1,36 @@
 <div>
-    L’ULB accorde une grande importance à la protection de vos données à caractère personnel.
+    <p>L’ULB accorde une grande importance à la protection de vos données à caractère personnel.</p>
 
-    En vous inscrivant sur le portail Stageoff de la Faculté de Pharmacie de l’Université libre de Bruxelles (ci-après « ULB »), vous consentez à ce que les données à caractère personnel collectées lors de l’inscription soient traitées par l’ULB afin d’examiner si les conditions requises pour recevoir un agrément comme maître de stages en officine à l’ULB sont remplies et, le cas échéant, vous mettre en contact avec un étudiant stagiaire de la Faculté de Pharmacie.
+    <p>En vous inscrivant sur le portail Stageoff de la Faculté de Pharmacie de l’Université libre de Bruxelles (ci-après « ULB »),
+        vous consentez à ce que les données à caractère personnel collectées lors de l’inscription soient traitées par l’ULB afin d’examiner si les conditions requises pour recevoir un agrément comme maître de stages en officine à l’ULB sont remplies et,
+        le cas échéant, vous mettre en contact avec un étudiant stagiaire de la Faculté de Pharmacie.
+    </p>
 
-    Vos données sont transmises à l’Ordre des Pharmaciens de Belgique afin de procéder à une vérification de votre situation avant l’octroi de l’agrément. Sauf pour répondre à une obligation légale, elles ne sont pas transmises à des tiers.
+    <p>Vos données sont transmises à l’Ordre des Pharmaciens de Belgique afin de procéder à une vérification de votre situation avant l’octroi de l’agrément.
+        Sauf pour répondre à une obligation légale, elles ne sont pas transmises à des tiers.
+    </p>
 
-    De plus, une fois votre agrément obtenu, vos données pourront être utilisées par l’ULB pour communiquer avec vous à propos des étudiants accueillis dans votre officine, notamment pour vous inviter à leur cérémonie de proclamation.
+    <p>De plus, une fois votre agrément obtenu, vos données pourront être utilisées par l’ULB pour communiquer avec vous à propos des étudiants accueillis dans votre officine,
+        notamment pour vous inviter à leur cérémonie de proclamation.
+    </p>
 
-    Les membres du personnel de l’ULB ayant accès à vos donnés sont tenus à une obligation de confidentialité, celle-ci implique notamment de :
+    <p>Les membres du personnel de l’ULB ayant accès à vos donnés sont tenus à une obligation de confidentialité, celle-ci implique notamment de :</p>
 
-    - ne pas accéder ou chercher à accéder à des données qui ne sont pas strictement nécessaires à l’exécution de leur fonction ;
+    <ul>
+        <li>ne pas accéder ou chercher à accéder à des données qui ne sont pas strictement nécessaires à l’exécution de leur fonction ;</li>
+        <li>ne pas divulguer, tant au cours de leur relation de travail qu’après la fin de celle-ci, toute donnée à caractère personnel dont ils auraient eu connaissance dans l’exercice de leur fonction, outre ce qui s’avérerait nécessaire pour l’exercice de celle-ci ;</li>
+        <li>ne conserver aucune copie des données traitées à l’issue de leur engagement à l’ULB.</li>
+    </ul>
 
-    - ne pas divulguer, tant au cours de leur relation de travail qu’après la fin de celle-ci, toute donnée à caractère personnel dont ils auraient eu connaissance dans l’exercice de leur fonction, outre ce qui s’avérerait nécessaire pour l’exercice de celle-ci ;
+    <p>Les étudiants ayant accès à vos données sont tenus à des obligations de confidentialité similaires.</p>
 
-    - ne conserver aucune copie des données traitées à l’issue de leur engagement à l’ULB.
+    <p> Vos données à caractère personnel seront conservées sur le portail Stageoff pendant une durée de 10 ans à partir de la date de fin de votre agrément et ce afin de permettre un suivi des stages déjà réalisés ainsi qu’une réinscription facilitée sur le portail.</p>
 
-    Les étudiants ayant accès à vos données sont tenus à des obligations de confidentialité similaires.
+    <p>Dans le cas où votre agrément serait refusé, vos données seront supprimées du portail Stageoff dans les 30 jours suivant la notification du refus</p>
 
-    Vos données à caractère personnel seront conservées sur le portail Stageoff pendant une durée de 10 ans à partir de la date de fin de votre agrément et ce afin de permettre un suivi des stages déjà réalisés ainsi qu’une réinscription facilitée sur le portail.
+    <p>Toutes vos questions sur la protection de vos données par l’ULB peuvent être envoyées à la Déléguée à la protection des données : rgpd@ulb.ac.be.</p>
 
-    Dans le cas où votre agrément serait refusé, vos données seront supprimées du portail Stageoff dans les 30 jours suivant la notification du refus.
+    <p>Cette adresse est également celle par le biais de laquelle vous pouvez exercer vos droits en la matière, en justifiant votre identité : accès, rectification, effacement, limitation, opposition et portabilité.</p>
 
-    Toutes vos questions sur la protection de vos données par l’ULB peuvent être envoyées à la Déléguée à la protection des données : rgpd@ulb.ac.be.
-
-    Cette adresse est également celle par le biais de laquelle vous pouvez exercer vos droits en la matière, en justifiant votre identité : accès, rectification, effacement, limitation, opposition et portabilité.
-
-    J’ai lu et j’affirme avoir compris dans son intégralité les dispositions relatives au traitement de mes données (RGPD)
+    <p>J’ai lu et j’affirme avoir compris dans son intégralité les dispositions relatives au traitement de mes données (RGPD).</p>
 </div>
