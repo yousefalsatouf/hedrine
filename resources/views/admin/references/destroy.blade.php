083e36683422322f0b1ea5f8d0467a4524949399
jpmpindu <jpmpindu@gmail.com>
2020-06-15 22:27:51 +0200

    change variable name reference to title

--- resources/views/admin/references/destroy.blade.php
+++ resources/views/admin/references/destroy.blade.php
@@ -9,7 +9,7 @@
       <div class="col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
         <div class="card text-white bg-light mb-3">
           <div class="card-body">
-            <h5 class="card-title text-center mb-3">Vous êtes sur le point de supprimer la référence "<strong>{{ $reference->name }}</strong>"</h5>
+            <h5 class="card-title text-center mb-3">Vous êtes sur le point de supprimer la référence "<strong>{{ $reference->title }}</strong>"</h5>
             <p class="card-text">
               <a class="btn btn-outline-danger btn-lg btn-block" href="#" role="button"
               onclick="event.preventDefault();
