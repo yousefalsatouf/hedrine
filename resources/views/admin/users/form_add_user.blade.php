fe8982e014623b3f1ce9ea78557966f6fc4c6731
jpmpindu <jpmpindu@gmail.com>
2020-06-25 00:36:11 +0200

    update for the modification of role user

--- resources/views/admin/users/form_add_user.blade.php
+++ resources/views/admin/users/form_add_user.blade.php
@@ -1,3 +1,4 @@
+
 @extends('dashboard.layout')
 
 <!-- @yield('content_title') créé dans la view master_dashboard.blade.php-->
@@ -17,69 +18,56 @@
 						</div>
 					@endif
 				    <div class="row">
+						<!-- left column -->
 						<div class="col-md-8 offset-md-2 ">
-							<!-- general form elements -->
-							<div class="card card-success">
-								<div class="card-header">
-									<h3 class="card-title">
-										<strong>
-											@if(Route::currentRouteName() === 'user.create')
-												Ajouter un nouveau user
-											@else
-												Modification d'un user
-											@endif 
-										</strong>
-									</h3>
-								</div>
-								<!-- /.card-header -->
-								<!-- form start -->
-								<form class=" justify-content-center" role="form" 
-										method="POST" action="@isset($user) {{ route('user.update', $user->id) }} @else {{route('user.store')}} @endisset">
-									<div class="card-body">
-										<div class="col-6">
-											<div class="form-group">
-												<label for="role_id">Route</label>
-												<select name="role_id" class="form-control">
-													@if(Route::currentRouteName() === 'user.create')
-															<option></option>
+						<!-- general form elements -->
+						<div class="card card-success">
+							<div class="card-header">
+								<h3 class="card-title">
+									<strong>
+										Modification d'une plante
+									</strong>
+								</h3>
+							</div>
+							<!-- /.card-header -->
+							<!-- form start -->
+						<form class=" justify-content-center" role="form" method="POST" action="@isset($user) {{ route('user.update', $user->id) }} @else {{
+							route('user.store')}} @endisset">
+								<div class="card-body">
+									<div class="form-group">
+										@isset($user) @method('PUT') @endisset
+										@csrf
+										<label for="role_id">Roles</label>
+											<select name="role_id" class="form-control">
+												<option></option>
 													@foreach ($roles as $role)
-														<option value="{{$role->id}}">{{$role->name}}</option>
+														<option value="{{$role->id}}" @if($user->role_id == $role->id) selected @endif>{{$role->name}}</option>
 													@endforeach
-													@else
-														<option></option>
-														@foreach ($roles as $role)
-															<option value="{{$role->id}}" @if($user->role_id == $role->id) selected @endif>{{$role->name}}</option>
-														@endforeach
-														@endif
-												</select>
-											</div>
-										</div>
-
-										
-									
-										<!-- /.card-body -->
-										<div class="card-footer">
-											<div class="control-group">
-												<div class="controls">
-													<a class="btn btn-light" href="{{ route('user.index') }}" role="button"><i class="fas fa-arrow-left"></i> Retour à la liste des users</a>
-													<button type="submit" class="btn btn-outline-success float-right"><i class="fas fa-location-arrow"></i>
-														@if(Route::currentRouteName() === 'user.create')
-															Ajouter un nouveau user
-														@else
-															Sauvegarder
-														@endif
-													</button>
-												</div>
-											</div>
+												
+											</select>
+									</div>
+                                    
+								</div>
+								<!-- /.card-body -->
+								<div class="card-footer">
+									<div class="control-group">
+										<div class="controls">
+											<a class="btn btn-light" href="{{ route('user.index') }}" role="button"><i class="fas fa-arrow-left"></i> Retour à la liste des utilisateurs</a>
+											<button type="submit" class="btn btn-outline-success float-right"><i class="fas fa-location-arrow"></i>
+												Sauvegarder
+										</button>
 										</div>
 									</div>
-								</form>
-					   		</div>
-					   	</div>
+								</div>
+							</form>
+					   </div>
+					   <!-- /.card -->
 					</div>
 				</div>
-			</section>
+			</div>
 		</div>
     </div>
+
+
 @endsection
 
