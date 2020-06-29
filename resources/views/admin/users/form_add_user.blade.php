3aad9a679bd7a81f19463e1dbbdf7daf4a04d281
jpmpindu <jpmpindu@gmail.com>
2020-06-29 11:21:50 +0200

    modification of user by admin

--- resources/views/admin/users/form_add_user.blade.php
+++ resources/views/admin/users/form_add_user.blade.php
@@ -25,7 +25,7 @@
 							<div class="card-header">
 								<h3 class="card-title">
 									<strong>
-										Modification d'une plante
+										Modification d'une plante 
 									</strong>
 								</h3>
 							</div>
@@ -34,19 +34,56 @@
 						<form class=" justify-content-center" role="form" method="POST" action="@isset($user) {{ route('user.update', $user->id) }} @else {{
 							route('user.store')}} @endisset">
 								<div class="card-body">
-									<div class="form-group">
-										@isset($user) @method('PUT') @endisset
-										@csrf
-										<label for="role_id">Roles</label>
-											<select name="role_id" class="form-control">
-												<option></option>
+									<div class="row">
+										<div class="col-6">
+											<div class="form-group">
+												@isset($user) @method('PUT') @endisset
+												@csrf
+												<label for="firstname">Prénom</label>
+												<input type="text" class="form-control" id="firstname" name="firstname" required placeholder="Veuillez indiquer un prénom pour l'utilisateur" value="{{isset($user) ? $user->firstname : ''}}">
+											</div>
+										</div>
+										<div class="col-6">
+											<div class="form-group">
+												<label for="name">Nom</label>
+												<input type="text" class="form-control" id="name" name="name" required placeholder="Veuillez indiquer un nom pour l'utilisateur" value="{{isset($user) ? $user->name : ''}}">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-6">
+											<div class="form-group">
+												<label for="tel1">Numéro Téléphome</label>
+												<input type="text" class="form-control" id="tel1" name="tel1" required placeholder="Veuillez indiquer un numéro pour l'utilisateur" value="{{isset($user) ? $user->tel1 : ''}}">
+											</div>
+										</div>
+										<div class="col-6">
+											<div class="form-group">
+												<label for="email">Email</label>
+												<input type="text" class="form-control" id="email" name="email" required placeholder="Veuillez indiquer un email pour l'utilisateur" value="{{isset($user) ? $user->email : ''}}">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-6">
+											<div class="form-group">
+												<label for="team">Team</label>
+												<input type="text" class="form-control" id="team" name="team" required placeholder="Veuillez indiquer une équipe pour l'utilisateur" value="{{isset($user) ? $user->team : ''}}">
+											</div>
+										</div>
+										<div class="col-6">
+											<div class="form-group">
+												<label for="role_id">Roles</label>
+												<select name="role_id" class="form-control">
+													<option></option>
 													@foreach ($roles as $role)
 														<option value="{{$role->id}}" @if($user->role_id == $role->id) selected @endif>{{$role->name}}</option>
 													@endforeach
-												
-											</select>
+														
+												</select>
+											</div>
+										</div>
 									</div>
-                                    
 								</div>
 								<!-- /.card-body -->
 								<div class="card-footer">
