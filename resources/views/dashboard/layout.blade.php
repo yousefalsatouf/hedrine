fb480fe78af97ab6b550334bbc472d4b138cea85
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-07-03 13:34:24 +0200

    make dropdown on hover replacing the click to make it easier

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -45,7 +45,7 @@
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="hold-transition sidebar-mini layout-fixed">
+<body class="hold-transition sidebar-mini layout-fixed" id="layout">
 <div class="wrapper">
 
     <!-- Navbar -->
